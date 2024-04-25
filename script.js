d3f7a07c2997fe47d852a62999b44e1545e3229d
Martin Jensen <mjmartinjensen@icloud.com>
2024-04-25 16:20:08 +0300

    change calculation process add error message and round 2 decimals
    theodinproject calculator #7

--- script.js
+++ script.js
@@ -1,51 +1,70 @@
-function addTwoNumbers(firstNumber, secondNumber) {
-    return firstNumber + secondNumber;
+function add(a, b) {
+    return +a + +b;
 }
 
-function subtractTwoNumbers(firstNumber, secondNumber) {
-    return firstNumber - secondNumber;
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiplyTwoNumbers(firstNumber, secondNumber) {
-    return firstNumber * secondNumber;
+function multiply(a, b) {
+    return a * b;
 }
 
-function divideTwoNumbers(firstNumber, secondNumber) {
-    return firstNumber / secondNumber;
+function divide(a, b) {
+    return a / b;
 }
 
-let firstNumberChosen;
-let operatorChosen;
-let secondNumberChosen;
-let result;
-
-function callOperationFunctions(operator, firstNumber, secondNumber) {
-    if(operator === "+") return addTwoNumbers(firstNumber, secondNumber);
-    if(operator === "-") return subtractTwoNumbers(firstNumber, secondNumber);
-    if(operator === "*") return multiplyTwoNumbers(firstNumber, secondNumber);
-    if(operator === "/") return divideTwoNumbers(firstNumber, secondNumber);
+function makeCalculation(operator, a, b) {
+    if(operator === "+") return add(a, b);
+    if(operator === "-") return subtract(a, b);
+    if(operator === "*") return multiply(a, b);
+    if(operator === "/") return divide(a, b);
 }
+
+let firstNumberInput = "0";
+let operatorInput = "+";
+let secondNumberInput = "0";
+let result = "0";
 
 const body = document.querySelector("body");
 const screen = document.querySelector(".screen");
 
 body.addEventListener("click", (event) => {
-    if (firstNumberChosen === undefined && typeof(+event.target.textContent) === "number") {
-        firstNumberChosen = +event.target.textContent;
-        screen.textContent = firstNumberChosen
-    } else if (firstNumberChosen != undefined && secondNumberChosen === undefined && (event.target.textContent === "+" || event.target.textContent === "-" || event.target.textContent === "*" || event.target.textContent === "/" )) {
-        operatorChosen = event.target.textContent;
-    } else if (firstNumberChosen != undefined && operatorChosen != undefined && secondNumberChosen === undefined && typeof(+event.target.textContent) === "number") {
-        secondNumberChosen = +event.target.textContent;
-        screen.textContent = secondNumberChosen
-    } else if (firstNumberChosen != undefined && operatorChosen != undefined && secondNumberChosen != undefined && event.target.textContent === "=") {
-        result = callOperationFunctions(operatorChosen, firstNumberChosen, secondNumberChosen);
-        screen.textContent = result
-    } else if (event.target.textContent === "AC") {
-        firstNumberChosen = undefined;
-        operatorChosen = undefined;
-        secondNumberChosen = undefined;
-        result = undefined;
-        screen.textContent = 0;
+    if (!isNaN(+event.target.textContent)) {
+        if (secondNumberInput === "0") {
+            secondNumberInput = event.target.textContent
+        } else {
+            secondNumberInput += event.target.textContent
+        }
+        screen.textContent = secondNumberInput;
     }
-})
+
+    if (event.target.textContent === "+" || 
+    event.target.textContent === "-" ||
+    event.target.textContent === "*" ||
+    event.target.textContent === "/") {
+        firstNumberInput = makeCalculation(operatorInput, firstNumberInput, secondNumberInput);
+        operatorInput = event.target.textContent;
+        secondNumberInput = "0";
+
+        firstNumberInput === Infinity ? screen.textContent = "Dumbass" : screen.textContent = +parseFloat(firstNumberInput).toFixed(2);
+    }
+
+    if (event.target.textContent === "=") {
+        result = makeCalculation(operatorInput, firstNumberInput, secondNumberInput);
+        firstNumberInput = "0";
+        operatorInput = "+";
+        secondNumberInput = "0";
+
+        result === Infinity ? screen.textContent = "Dumbass" : screen.textContent = +parseFloat(result).toFixed(2);
+    }
+
+    if (event.target.textContent === "AC") {
+        firstNumberInput = "0";
+        operatorInput = "+";
+        secondNumberInput = "0";
+        result = "0";
+        screen.textContent = "0";
+    }
+
+});
